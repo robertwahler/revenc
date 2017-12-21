ff99e1bf8a926056cd94ce5936aef89178084e7c
Robert Wahler <robert@gearheadforhire.com>
2017-12-21 10:52:24 -0500

    use RbConfig instead of Config

--- lib/revenc.rb
+++ lib/revenc.rb
@@ -25,8 +25,8 @@ module Revenc
 
   # Platform constants
   unless defined?(Revenc::WINDOWS)
-    WINDOWS = Config::CONFIG['host_os'] =~ /mswin|mingw/i
-    CYGWIN = Config::CONFIG['host_os'] =~ /cygwin/i
+    WINDOWS = RbConfig::CONFIG['host_os'] =~ /mswin|mingw/i
+    CYGWIN = RbConfig::CONFIG['host_os'] =~ /cygwin/i
   end
 
 end
